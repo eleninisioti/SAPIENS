a776821477952cd126c7ba5f89dceba0df0890eb
Eleni <e.nisioti@essex.ac.uk>
2022-06-01 17:15:58 +0200

    notebook fix

--- lib/wordcraft/utils/task_utils.py
+++ lib/wordcraft/utils/task_utils.py
@@ -1,21 +1,21 @@
 env_name = "wordcraft-multistep-no-goal-v0"
 
 
-recipe_book_info = {"single path": {"path": "recipe_books/1path.json",
+recipe_book_info = {"single_path": {"path": "recipe_books/single_path.json",
                                     "best_paths": ["a_8"],
                                     "npaths": 1,
                                     "best_reward": 36,
                                     "max_steps": 1000000,
                                     "early_step": 500000},
 
-                    "merging paths": {"path": "recipe_books/cross_easier.json",
+                    "merging_paths": {"path": "recipe_books/merging_paths.json",
                                       "npaths": 3,
                                       "best_reward": 50,
                                       "max_steps": 1000000,
                                       "early_step": 500000,
                                       "best_paths": ["a_8", "b_8", "c_10"]},
 
-                    "best_of_ten": {"path": "recipe_books/10_mirror.json",
+                    "bestoften_paths": {"path": "recipe_books/bestof10_paths.json",
                                     "npaths": 10,
                                     "best_reward": 72,
                                     "max_steps": 3500000,
