6e1dbab66db5cd56797f6d90cfd714b98ae0713d
Eleni <e.nisioti@essex.ac.uk>
2022-10-05 14:45:24 +0200

    msmall fix

--- server_scripts/experiments.py
+++ server_scripts/experiments.py
@@ -6,7 +6,7 @@ sys.path.append(project_path)
 project_path = "/gpfsssd/scratch/rech/imi/utw61ti/workspace/SAPIENS"
 sys.path.append(project_path)
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 
 print(sys.path)
