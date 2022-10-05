8f80f5e79e3cf4b492c6eb226144a958eedeed71
Eleni <e.nisioti@essex.ac.uk>
2022-10-05 14:25:56 +0200

    msmall fix

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -6,7 +6,7 @@ project_path = "/gpfsscratch/rech/imi/utw61ti/workspace/SAPIENS"
 sys.path.insert(0,project_path)
 __file__ = "scripts.evaluate"
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 print(sys.path)
 import gym
 import copy
