83650756a926f7c263605321b311a4129a8e4c9b
Eleni <e.nisioti@essex.ac.uk>
2022-10-05 13:45:40 +0200

    small fix

--- scripts/server/utils.py
+++ scripts/server/utils.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-calling_dir = os.path.dirname(os.path.realpath(__file__))
+calling_dir = os.getcwd()
 sys.path.append(calling_dir)
 
 def run_server(job_name, trial, gpu=False, time="20:00:00", long_run=False, account="jeanzay_id"):
